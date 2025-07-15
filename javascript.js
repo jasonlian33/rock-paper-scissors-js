bd2aa6e8890607b18bec9f3c8ca31dbe6da55dad
jason lian <jasonlian16@gmail.com>
2025-07-14 21:41:48 -0400

    implemented getComputerChoice using Math.random

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
 console.log("hello!")
+
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random() * 3); // randomly chooses a number 0,1,2
+    console.log(randomNum);
+
+    if (randomNum === 0){
+        return "Rock";
+    } else if (randomNum === 1){
+        return "Paper";
+    } else{
+        return "Scissors";
+    }
+}
+
